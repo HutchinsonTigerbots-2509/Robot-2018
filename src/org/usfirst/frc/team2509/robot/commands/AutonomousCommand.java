8aacf08d34da5af5bc28564c6264f7eb4a90febf
Erstaunlich <mason.deming@gmail.com>
2018-01-16 16:45:01 -0600

    Added Encoder
    ok i kinda just stole dj's made a few edits, this is just initial push, will add the actual path and stuff later

--- src/org/usfirst/frc/team2509/robot/commands/AutonomousCommand.java
+++ src/org/usfirst/frc/team2509/robot/commands/AutonomousCommand.java
@@ -1,13 +1,26 @@
 package org.usfirst.frc.team2509.robot.commands;
 
+import org.usfirst.frc.team2509.robot.RobotMap;
+import org.usfirst.frc.team2509.robot.subsystems.DriveTrain;
+
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
-import org.usfirst.frc.team2509.robot.*;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 /**
  *
  */
 public class AutonomousCommand extends Command {
-
-    public AutonomousCommand() {
+	DifferentialDrive DT=RobotMap.Drive;
+	SpeedControllerGroup Left=RobotMap.DTG_LEFT;
+	SpeedControllerGroup Right=RobotMap.DTG_RIGHT;
+	ADXRS450_Gyro gyro = RobotMap.gyro;
+	Encoder enc1 = RobotMap.enc1;
+	Encoder enc2 = RobotMap.enc2;
+	
+	public AutonomousCommand() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
     	
@@ -15,8 +28,14 @@ public class AutonomousCommand extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
-    
-    
+    	gyro.reset();
+    	enc1.reset();
+    	enc2.reset();
+    	Timer.delay(0.1);
+    	int targetDistance = 2872*3;
+    	while(((enc1.get()-enc2.get())/2)<=targetDistance) {
+    		DriveTrain.arcadeDrive(0.75, gyro.getAngle()*(0.03));//0.03
+    	}
     
     
     }
