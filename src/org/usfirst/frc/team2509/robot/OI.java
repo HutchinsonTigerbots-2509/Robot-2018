a5246dbee1c96e66aece64622f624c21e890f4a8
GoldenGollem <goldengollem@gmail.com>
2018-01-17 17:22:25 -0600

    Revert "Merge branch 'DJ-drivetrain' into master"
    This reverts commit d720e606ba755fe17efe22c1499354301412265d, reversing
    changes made to 420df650fefdd6a693e104d7008f44d2d99258cd.

--- src/org/usfirst/frc/team2509/robot/OI.java
+++ src/org/usfirst/frc/team2509/robot/OI.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /*----------------------------------------------------------------------------*/
 /* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
@@ -43,3 +44,63 @@ public class OI {
 		
 	}
 }
+=======
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.usfirst.frc.team2509.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**
+ * This class is the glue that binds the controls on the physical operator
+ * interface to the commands and command groups that allow control of the robot.
+ */
+public class OI {
+	public Joystick OperatorStick;
+	public Joystick CoOperatorStick;
+	
+	/**
+	 * CREATING BUTTONS - 
+	 * <p>One type of button is a joystick button which is any button on a joystick.
+	 * You create one by telling it which joystick it's on and which button number it is.
+	 * <p>Joystick stick = new Joystick(port);
+	 * <p>Button button = new JoystickButton(stick, buttonNumber);
+	 * <p>There are a few additional built in buttons you can use. Additionally,
+	 * by subclassing Button you can create custom triggers and bind those to
+	 * commands the same as any other Button.
+	 * 
+	 * <p>TRIGGERING COMMANDS WITH BUTTONS
+	 * <p>Once you have a button, it's trivial to bind it to a button in one of three ways:
+	 * <p>Start the command when the button is pressed and let it run the command until 
+	 * it is finished as determined by it's isFinished method.
+	 * <p>button.whenPressed(new ExampleCommand());
+	 * <p>Run the command while the button is being held down and interrupt it once the 
+	 * button is released.
+	 * <p>button.whileHeld(new ExampleCommand());
+	 * <p>Start the command when the button is released and let it run the command until 
+	 * it is finished as determined by it's isFinished method.
+	 * <p>button.whenReleased(new ExampleCommand());
+	 * 
+	 */
+	public OI() {
+		OperatorStick = new Joystick(0);
+		CoOperatorStick = new Joystick(1);
+	}
+	/**
+	 * When called constantly updates the SmartDashboard
+	 */
+	public Thread UpdateDashboard = new Thread(()->{
+		while(true) {
+			SmartDashboard.putNumber("Left Encoder", Robot.drivetrain.getLeftEncoder().get());
+			SmartDashboard.putNumber("Right Encoder", Robot.drivetrain.getRightEncoder().get());
+			SmartDashboard.putNumber("Gyro", Robot.drivetrain.getGyro().getAngle());
+		}
+	});
+}
+>>>>>>> parent of d720e60... Merge branch 'DJ-drivetrain' into master
