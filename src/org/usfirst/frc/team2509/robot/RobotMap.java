44af8fdcf9b7d6c89278a4444237aa08e69e6393
Tigerbots2509-Member <nathanchristensen9@student.isd423.org>
2018-01-23 15:34:44 -0600

    New Branch
    there was some formatting stuff that had created issues, im remaking my auto branch

--- src/org/usfirst/frc/team2509/robot/RobotMap.java
+++ src/org/usfirst/frc/team2509/robot/RobotMap.java
@@ -7,12 +7,13 @@
 
 package org.usfirst.frc.team2509.robot;
 
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * The RobotMap is a mapping from the ports sensors and actuators are wired into
@@ -21,29 +22,66 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
  * floating around.
  */
 public class RobotMap {
-	
-	public static WPI_TalonSRX DT_LEFT1 = new WPI_TalonSRX(0);
-	public static WPI_TalonSRX DT_LEFT2 = new WPI_TalonSRX(1);
-	public static WPI_TalonSRX DT_RIGHT1 = new WPI_TalonSRX(2);
-	public static WPI_TalonSRX DT_RIGHT2 = new WPI_TalonSRX(3);
-	
-	public static SpeedControllerGroup DTG_LEFT = new SpeedControllerGroup(DT_LEFT1, DT_LEFT2);
-	public static SpeedControllerGroup DTG_RIGHT = new SpeedControllerGroup(DT_RIGHT1, DT_LEFT2);
-	
-	public static DifferentialDrive Drive = new DifferentialDrive(DTG_LEFT, DTG_RIGHT);
-	public static Encoder enc1 = new Encoder(0, 1, false, Encoder.EncodingType.k4X);
-	public static Encoder enc2 = new Encoder(2, 3, false, Encoder.EncodingType.k4X);
-	public static ADXRS450_Gyro gyro = new ADXRS450_Gyro();
-	// For example to map the left and right motors, you could define the
-	// following variables to use with your drivetrain subsystem.
-	// public static int leftMotor = 1;
-	// public static int rightMotor = 2;
 
-	// If you are using multiple modules, make sure to define both the port
-	// number and the module. For example you with a rangefinder:
-	// public static int rangefinderPort = 1;
-	// public static int rangefinderModule = 1;
-	public static void init() {
+	//Drivetran Variable
+	public static DoubleSolenoid DT_Shifter;
+	public static Encoder DT_LeftEncoder;
+	public static Encoder DT_RightEncoder;
+	public static ADXRS450_Gyro DT_Gyro;
+	public static Talon DT_LEFT1;
+	public static Talon DT_LEFT2;
+	public static Talon DT_LEFT3;
+	public static Talon DT_RIGHT1;
+	public static Talon DT_RIGHT2;
+	public static Talon DT_RIGHT3;
+	public static SpeedControllerGroup DTG_LEFT;
+	public static SpeedControllerGroup DTG_RIGHT;
+	public static DifferentialDrive RobotDrive;
+	
+	/**
+	 * 
+	 */
+	public static void init(){
+		//Drivetrain Variable Initialize
+		DT_Shifter = new DoubleSolenoid(0,1);
+		
+		DT_LeftEncoder = new Encoder(0,1);
+		SmartDashboard.putNumber("Left Encoder", DT_LeftEncoder.get());
+		
+		DT_RightEncoder = new Encoder(2,3);
+		SmartDashboard.putNumber("Right Encoder", DT_RightEncoder.get());
+		
+		
+		DT_Gyro = new ADXRS450_Gyro();
+		DT_Gyro.reset();
+		DT_Gyro.calibrate();
+		SmartDashboard.putNumber("Gyro", DT_Gyro.getAngle());
+		
+		DT_LEFT1 = new Talon(0);
+		
+		DT_LEFT2 = new Talon(2);
+		
+		DT_LEFT3 = new Talon(1);
+		
+		DT_RIGHT1 = new Talon(3);
+		
+		DT_RIGHT2 = new Talon(4);
+		
+		DT_RIGHT3 = new Talon(5);
+		
+		//DTG because its a drivetrain(DT) group
+		DTG_LEFT = new SpeedControllerGroup(
+				DT_LEFT1,DT_LEFT2,DT_LEFT3);
+		
+		DTG_RIGHT = new SpeedControllerGroup(
+				DT_RIGHT1,DT_RIGHT2,DT_RIGHT3);
+		
+		RobotDrive = new DifferentialDrive(DTG_LEFT,DTG_RIGHT);
+		
+	}
+
+	private static void whenPressed() {
+		// TODO Auto-generated method stub
 		
 	}
 }
--- src/org/usfirst/frc/team2509/robot/subsystems/DriveTrain.java
+++ src/org/usfirst/frc/team2509/robot/subsystems/DriveTrain.java
@@ -2,48 +2,188 @@ package org.usfirst.frc.team2509.robot.subsystems;
 
 import org.usfirst.frc.team2509.robot.RobotMap;
 
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-
 
 /**
  *
  */
 public class DriveTrain extends Subsystem {
-	WPI_TalonSRX LEFT1 = RobotMap.DT_LEFT1;
-	WPI_TalonSRX LEFT2 = RobotMap.DT_LEFT2;
-	WPI_TalonSRX RIGHT1 = RobotMap.DT_RIGHT1;
-	WPI_TalonSRX RIGHT2 = RobotMap.DT_RIGHT2;
-	Encoder enc = RobotMap.enc1; 
-	SpeedControllerGroup DTG_LEFT = RobotMap.DTG_LEFT;
-	SpeedControllerGroup DTG_RIGHT = RobotMap.DTG_RIGHT;
-	
-	DifferentialDrive Drive = RobotMap.Drive;
-	
-
+	//Subsystem Variables
+	private static DoubleSolenoid Shifter = RobotMap.DT_Shifter;
+	private static Encoder LeftEncoder = RobotMap.DT_LeftEncoder;
+	private static Encoder RightEncoder = RobotMap.DT_RightEncoder;
+	private static ADXRS450_Gyro Gyro = RobotMap.DT_Gyro;
+	private static Talon Left_1 = RobotMap.DT_LEFT1;
+	private static Talon Left_2 = RobotMap.DT_LEFT2;
+	private static Talon Left_3 = RobotMap.DT_LEFT3;
+	private static Talon Right_1 = RobotMap.DT_RIGHT1;
+	private static Talon Right_2 = RobotMap.DT_RIGHT2;
+	private static Talon Right_3 = RobotMap.DT_RIGHT3;
+	private static SpeedControllerGroup Left = RobotMap.DTG_LEFT;
+	private static SpeedControllerGroup Right = RobotMap.DTG_RIGHT;
+	private static DifferentialDrive Drive = RobotMap.RobotDrive;
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
-
-	public void drive(Joystick STICK) {
-		Drive.arcadeDrive(STICK.getY(), STICK.getZ());
+    public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
     }
+    public void drive(Joystick stick) {
+    	Drive.arcadeDrive(stick.getY(), stick.getZ());
+    }
+    /**
+     * Resets all sensors
+     */
+    public void sensorReset() {
+    	Gyro.reset();
+    	RightEncoder.reset();
+    	LeftEncoder.reset();
+    }
+    /**
+     * Gets angle from the Gyro to tell the motors to make a specific turn
+     * @param Angle
+     */
+    public void rotate(double targetAngle) {
+    	Gyro.reset();
+    	Timer.delay(0.05);
+    	if(Gyro.getAngle()<targetAngle) {
+    		while(Gyro.getAngle()<=targetAngle)	Drive.tankDrive(-0.55, 0.55);
+    		Drive.tankDrive(0, 0);
+    	}else if(Gyro.getAngle()>targetAngle) {
+    		while(Gyro.getAngle()>=targetAngle)Drive.tankDrive(0.55, -0.55);
+    		Drive.tankDrive(0, 0);
+    	}else {
+    		Drive.tankDrive(0, 0);
+    	}
+    }
 
-	@Override
-	protected void initDefaultCommand() {
-		// TODO Auto-generated method stub
-		
-	}
+    /**
+     * Switches the gear between high and low, with a double solenoid.
+     * @param isExtended
+     */
+    public void shift(boolean isExtended) {
+    	if(isExtended) {
+    		Shifter.set(DoubleSolenoid.Value.kForward);
+    	}
+    	else {
+    		Shifter.set(DoubleSolenoid.Value.kReverse);
+    	}
+    }
+    
+    /**
+     * When called will drive forward.
+     * You need to input inches as a target value.
+     * @param targetDistance
+     */
+    public void driveForward(double targetDistance) {
+    	sensorReset();
+    	double wheelDiameter = 6;
+    	double target = (targetDistance/(wheelDiameter*Math.PI))*3*360;
+    	Timer.delay(0.1);
+    	while((RightEncoder.get()-LeftEncoder.get())/2<=target) {
+    		Drive.arcadeDrive(0.5, Gyro.getAngle()*(0.15));
+    	}
+    	Drive.tankDrive(0, 0);
+    }
+    
+    /**
+     * 
+     * @return DriveTrain_Shifter
+     */
+    public DoubleSolenoid getShifter() {
+    	return Shifter;
+    }
+    /**
+     * 
+     * @return DriveTrain_LeftEncoder
+     */
+    public Encoder getLeftEncoder() {
+    	return LeftEncoder;
+    }
+    /**
+     * 
+     * @return DriveTrain_RightEncoder
+     */
+    public Encoder getRightEncoder() {
+    	return RightEncoder;
+    }
+    /**
+     * 
+     * @return DriveTrain_Gyro
+     */
+    public ADXRS450_Gyro getGyro() {
+    	return Gyro;
+    }
+    /**
+     * 
+     * @return DriveTrain_Left
+     */
+    public SpeedControllerGroup getLeft() {
+    	return Left;
+    }
+    /**
+     * 
+     * @return DriveTrain_Right
+     */
+    public SpeedControllerGroup getRight() {
+    	return Right;
+    }
+    /**
+     * 
+     * @return DriveTrain_Drive
+     */
+    public DifferentialDrive getDrive() {
+    	return Drive;
+    }
+    /**
+     * 
+     * @return DriveTrain_Left_1
+     */
+    public Talon getLeft1() {
+    	return Left_1;
+    }
+    /**
+     * 
+     * @return DriveTrain_Left_2
+     */
+    public Talon getLeft2() {
+    	return Left_2;
+    }
+    /**
+     * 
+     * @return DriveTrain_Left_3
+     */
+    public Talon getLeft3() {
+    	return Left_3;
+    }
+    /**
+     * 
+     * @return DriveTrain_Right_1
+     */
+    public Talon getRight1() {
+    	return Right_1;
+    }
+    /**
+     * 
+     * @return DriveTrain_Right_2
+     */
+    public Talon getRight2() {
+    	return Right_2;
+    }
+    /**
+     * 
+     * @return DriveTrain_Right_3
+     */
+    public Talon getRight3() {
+    	return Right_3;
+    }
 
-	public static void arcadeDrive(double d, double e) {
-		// TODO Auto-generated method stub
-		
-	}
 }
-
