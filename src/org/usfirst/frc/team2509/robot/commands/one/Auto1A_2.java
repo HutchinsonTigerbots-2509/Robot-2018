88945582277382c3c859683e64829dd906659b26
DJ's Laptop <danielscheele1@student.isd423.org>
2018-03-01 16:50:44 -0600

    Fixed some autos 3D is ready to be tested

---
+++ src/org/usfirst/frc/team2509/robot/commands/one/Auto1A_2.java
@@ -0,0 +1,48 @@
+package org.usfirst.frc.team2509.robot.commands.one;
+
+import org.usfirst.frc.team2509.robot.commands.Down;
+import org.usfirst.frc.team2509.robot.commands.DriveForward;
+import org.usfirst.frc.team2509.robot.commands.DriveTurn;
+import org.usfirst.frc.team2509.robot.commands.armThreadMid;
+import org.usfirst.frc.team2509.robot.commands.driveBackward;
+import org.usfirst.frc.team2509.robot.commands.extendUpper;
+import org.usfirst.frc.team2509.robot.commands.retract;
+import org.usfirst.frc.team2509.robot.commands.retractUpper;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class Auto1A_2 extends CommandGroup {
+
+    public Auto1A_2() {
+        // Add Commands here:
+        // e.g. addSequential(new Command1());
+        //      addSequential(new Command2());
+        // these will run in order.
+
+        // To run multiple commands at the same time,
+        // use addParallel()
+        // e.g. addParallel(new Command1());
+        //      addSequential(new Command2());
+        // Command1 and Command2 will run in parallel.
+
+        // A command group will require all of the subsystems that each member
+        // would require.
+        // e.g. if Command1 requires chassis, and Command2 requires arm,
+        // a CommandGroup containing them would require both the chassis and the
+        // arm.
+    	
+    	addSequential(new retractUpper());
+    	addSequential(new DriveForward(75));
+    	addSequential(new DriveTurn(90));
+    	addSequential(new armThreadMid());
+    	addSequential(new DriveForward(45));
+    	addSequential(new DriveTurn(-90));
+    	addSequential(new retract());
+    	addSequential(new driveBackward(15));
+    	addSequential(new Down());
+    	addSequential(new extendUpper());
+    }
+}
