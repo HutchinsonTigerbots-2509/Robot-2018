7fe9eeabe164b9aa8ba9bfea1141d9634c28d1ab
DJ's Laptop <danielscheele1@student.isd423.org>
2018-03-02 15:17:46 -0600

    made dropbox command

--- src/org/usfirst/frc/team2509/robot/commands/Drop_Box.java
+++ src/org/usfirst/frc/team2509/robot/commands/Drop_Box.java
@@ -24,6 +24,7 @@ public class Drop_Box extends CommandGroup {
         // e.g. if Command1 requires chassis, and Command2 requires arm,
         // a CommandGroup containing them would require both the chassis and the
         // arm.
+    	
     	addSequential(new WristDown());
     	addSequential(new OpenGripper());
     	addSequential(new WristUp());
